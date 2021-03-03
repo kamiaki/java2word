60e09384ea30e443f1e8c27b863ce66cdfc5c73c
kamiaki <247557768@qq.com>
2021-03-03 14:36:37 +0800

    Initial commit

---
+++ src/main/java/com/aki/java2word/Java2wordApplication.java
@@ -0,0 +1,13 @@
+package com.aki.java2word;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java2wordApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Java2wordApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aki/java2word/Java2wordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aki.java2word;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java2wordApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
